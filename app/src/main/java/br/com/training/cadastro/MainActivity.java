edc6683eae944518344dde66dcde5a7e66e5f91b
fabiobmarques <fabio.bat.marques@gmail>
2016-02-27 17:44:32 -0300

    Adicionar ciclo de vida

--- app/src/main/java/br/com/training/cadastro/MainActivity.java
+++ app/src/main/java/br/com/training/cadastro/MainActivity.java
@@ -1,31 +1,58 @@
 package br.com.training.cadastro;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNome;
+    EditText editTextIdade;
+    EditText editTextTelefone;
+    final String TAG="MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        editTextNome = (EditText) findViewById(R.id.editTextNome);
+        editTextIdade = (EditText) findViewById(R.id.editTextIdade);
+        editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String nome = editTextNome.getText().toString();
+                String idade = editTextIdade.getText().toString();
+                String telefone = editTextTelefone.getText().toString();
+                //chamar outra tela = tela atual.this,tela que quero ir.class
+                Intent intent = new Intent(MainActivity.this,Tela2Activity.class);
+                intent.putExtra("nome",nome);
+                intent.putExtra("idade",idade);
+                intent.putExtra("tel",telefone);
+                startActivity(intent);
+                finish();
             }
         });
+
+        Log.i(TAG,"onCreate");
     }
 
     @Override
@@ -48,5 +75,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
     }
 }
